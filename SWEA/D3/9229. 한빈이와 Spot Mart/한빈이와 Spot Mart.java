d13631109de61857b0a3f4733a528e264d3ab23a
zzckckck3 <65270703+zzckckck3@users.noreply.github.com>
2023-02-14 17:47:59 +0900

    [D3] Title: 한빈이와 Spot Mart, Time: 236 ms, Memory: 33,668 KB -BaekjoonHub

--- SWEA/D3/9229. 한빈이와 Spot Mart/한빈이와 Spot Mart.java
+++ SWEA/D3/9229. 한빈이와 Spot Mart/한빈이와 Spot Mart.java
@@ -23,7 +23,13 @@ public class Solution {
 			int max = 0;
 			int sum = 0;
 			for (int i = 0; i < N - 1; i++) {
+				if(arr[i] > M) {
+					continue;
+				}
 				for (int j = i + 1; j < N; j++) {
+					if (arr[j] > M) {
+						continue;
+					}
 					sum = arr[i] + arr[j];
 					if(sum <= M && max < sum) {
 						max = sum;
